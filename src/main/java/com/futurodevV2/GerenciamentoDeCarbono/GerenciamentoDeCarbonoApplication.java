1655e96a66a6f0fa9df3d9c9091a01057725b9de
Franciane SL <fran.schier@outlook.com>
2025-05-05 21:36:01 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/futurodevV2/GerenciamentoDeCarbono/GerenciamentoDeCarbonoApplication.java
@@ -0,0 +1,13 @@
+package com.futurodevV2.GerenciamentoDeCarbono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeCarbonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeCarbonoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futurodevV2/GerenciamentoDeCarbono/GerenciamentoDeCarbonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurodevV2.GerenciamentoDeCarbono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeCarbonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
