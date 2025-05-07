bdb7e49435eb5eb8d886f1e9fb56673533b340a3
Franciane SL <fran.schier@outlook.com>
2025-05-07 19:11:29 -0300

    Implementação da Entity Material

---
+++ src/main/java/Model/Entity/Material.java
@@ -0,0 +1,22 @@
+package Model.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table (name = "materiais")
+public class Material {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private double percentualDeCompensacao;
+}
