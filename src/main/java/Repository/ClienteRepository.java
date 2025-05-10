a35518783f9e1187bc5ac894f1c510c1f2fc95eb
Franciane SL <fran.schier@outlook.com>
2025-05-10 19:29:26 -0300

    Inserção da anotação @Repository

--- src/main/java/Repository/ClienteRepository.java
+++ src/main/java/Repository/ClienteRepository.java
@@ -2,7 +2,9 @@ package Repository;
 
 import Model.Entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
 }
