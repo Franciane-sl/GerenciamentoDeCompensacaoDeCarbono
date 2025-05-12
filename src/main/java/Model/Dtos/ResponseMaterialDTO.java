852e27d63d23dab4ede692b08c2bae34fd0db7c8
Franciane SL <fran.schier@outlook.com>
2025-05-12 20:35:48 -0300

    Implementação do DTO ResponseMaterialDTO

---
+++ src/main/java/Model/Dtos/ResponseMaterialDTO.java
@@ -0,0 +1,15 @@
+package Model.Dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseMaterialDTO {
+
+    private Long id;
+    private String nome;
+    private double  percentualDeCompensacao;
+}
