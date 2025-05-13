ab538bd9d87858ba294cfc95440251bde9b81ea1
Franciane SL <fran.schier@outlook.com>
2025-05-12 21:50:11 -0300

    Implementação do método para consultar todos os clientes no ClienteService.

--- src/main/java/Service/ClienteService.java
+++ src/main/java/Service/ClienteService.java
@@ -1,12 +1,19 @@
 package Service;
 
+import Model.Entity.Cliente;
 import Repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClienteService {
 
     @Autowired
     ClienteRepository clienteRepository;
+
+   public List<Cliente> findAllClientes(){
+       return clienteRepository.findAll();
+    }
 }
