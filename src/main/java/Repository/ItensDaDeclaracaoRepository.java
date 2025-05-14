7e354ed664a0e66ce14ec8078ca2346797973568
Franciane SL <fran.schier@outlook.com>
2025-05-14 19:30:18 -0300

    Implementação do método para filtrar os itens com base no clienteId e no intervalo de datas.

--- src/main/java/Repository/ItensDaDeclaracaoRepository.java
+++ src/main/java/Repository/ItensDaDeclaracaoRepository.java
@@ -2,9 +2,23 @@ package Repository;
 
 import Model.Entity.ItensDaDeclaracao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface ItensDaDeclaracaoRepository extends JpaRepository<ItensDaDeclaracao, Long> {
 
+    @Query("SELECT i FROM ItensDaDeclaracao i " +
+            "WHERE i.declaracao.cliente.id = :clienteId " +
+            "AND i.declaracao.dataInicialDoPeriodo >= :dataInicial " +
+            "AND i.declaracao.dataFinalDoPeriodo <= :dataFinal ")
+    List<ItensDaDeclaracao> findByClienteEPeriodo(
+            @Param("clienteId") Long clienteID,
+            @Param("dataInicial") LocalDate dataInicial,
+            @Param("dataFinal") LocalDate dataFinal
+            );
 }
