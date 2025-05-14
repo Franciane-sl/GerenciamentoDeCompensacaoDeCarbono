4d4759e775a654806e91ba23b651a5015117a731
Franciane SL <fran.schier@outlook.com>
2025-05-14 11:10:38 -0300

    Implementação do método para listar declaração pelo id.

--- src/main/java/Service/DeclaracaoService.java
+++ src/main/java/Service/DeclaracaoService.java
@@ -1,6 +1,7 @@
 package Service;
 
 import Model.Entity.Declaracao;
+import Model.Exceptions.ResourceNotFoundException;
 import Repository.DeclaracaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,11 @@ public class DeclaracaoService {
 
     public List<Declaracao> findAllDeclaracao(){
         return declaracaoRepository.findAll();
+    }
+
+    public Declaracao findDeclaracaoById (Long id){
+       return declaracaoRepository.findById(id).orElseThrow(
+               () -> new ResourceNotFoundException("A declaração não foi encontrada pelo id " + id)
+       );
     }
 }
