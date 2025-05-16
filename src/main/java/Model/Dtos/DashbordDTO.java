2c6fb4158445fe80119f02d38ddd1afcd3fe7634
Franciane SL <fran.schier@outlook.com>
2025-05-15 21:23:46 -0300

    Implementação do DTO DashbordDTO.

---
+++ src/main/java/Model/Dtos/DashbordDTO.java
@@ -0,0 +1,12 @@
+package Model.Dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DashbordDTO {
+
+    private String nomeMaterial;
+    private double totalToneladasCompensadas;
+}
