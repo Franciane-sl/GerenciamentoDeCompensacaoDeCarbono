d45436da97a47e900e3a545ba28cac86312856f4
Franciane SL <fran.schier@outlook.com>
2025-05-18 18:48:57 -0300

    Correção do nome do endpoint.

--- src/main/java/com/futurodevV2/GerenciamentoDeCarbono/Controller/DeclaracaoController.java
+++ src/main/java/com/futurodevV2/GerenciamentoDeCarbono/Controller/DeclaracaoController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/declaracao")
+@RequestMapping("/declaracoes")
 public class DeclaracaoController {
 
     @Autowired
