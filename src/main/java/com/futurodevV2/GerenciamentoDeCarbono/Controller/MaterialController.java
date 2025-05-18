1fb3fb827fa74079d3453d1cd34ad0611b75f1b0
Franciane SL <fran.schier@outlook.com>
2025-05-18 18:51:53 -0300

    Correção do nome do endpoint.

--- src/main/java/com/futurodevV2/GerenciamentoDeCarbono/Controller/MaterialController.java
+++ src/main/java/com/futurodevV2/GerenciamentoDeCarbono/Controller/MaterialController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/material")
+@RequestMapping("/materiais")
 public class MaterialController {
 
     @Autowired
