20b3f354f14bd89c664c5c4786396b37ed2a3b5e
Franciane SL <fran.schier@outlook.com>
2025-05-18 18:46:14 -0300

    Correção do nome do endpoint.

--- src/main/java/com/futurodevV2/GerenciamentoDeCarbono/Controller/DashboardController.java
+++ src/main/java/com/futurodevV2/GerenciamentoDeCarbono/Controller/DashboardController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/dashboard")
+@RequestMapping("/dashboards")
 public class DashboardController {
 
     @Autowired
