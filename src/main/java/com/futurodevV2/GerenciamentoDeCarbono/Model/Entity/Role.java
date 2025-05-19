849740b6d30c7ed15c64f16de167032fcb6ace1e
Franciane SL <fran.schier@outlook.com>
2025-05-19 10:51:23 -0300

    Implementação da entidade Role.

---
+++ src/main/java/com/futurodevV2/GerenciamentoDeCarbono/Model/Entity/Role.java
@@ -0,0 +1,20 @@
+package com.futurodevV2.GerenciamentoDeCarbono.Model.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
